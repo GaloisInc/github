018de9e18be8386c9d6d1e540d9d3217cf800849
Matt Bauer <mbauer@galois.com>
2019-05-24 15:56:26 -0400

    Enable reactions in PR comments

--- src/GitHub.hs
+++ src/GitHub.hs
@@ -222,6 +222,9 @@ module GitHub (
     -- * Delete a comment
     pullRequestCommentsR,
     pullRequestCommentR,
+    pullRequestCommentsWithReactionsR,
+    pullRequestCommentsWithReactionsIO,
+    pullRequestCommentsIO,
     createPullCommentR,
     createPullCommentReplyR,
 
